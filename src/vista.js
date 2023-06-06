6d9d53e18b6b372826ab4685d56016003e086cc1
Tomas Lopez <tomas.alp07@gmail.com>
2023-06-06 11:26:24 -0300

    Update vista.js

--- src/vista.js
+++ src/vista.js
@@ -11,39 +11,62 @@ function App() {
   }, []);
 
   const renderProductos = () => {
-  const productosOrdenados = productos.sort((a, b) => a.nombre.localeCompare(b.nombre));
-  const productosRenderizados = [];
+    const productosOrdenados = productos.sort((a, b) => a.nombre.localeCompare(b.nombre));
+    console.log(productosOrdenados);
+    const productosRenderizados = [];
 
-  let nombreActual = '';
-  productosOrdenados.forEach(producto => {
-    if (producto.nombre !== nombreActual) {
-      // Mostrar nombre y descripción
-      productosRenderizados.push(
-        <li key={producto.id}><strong>{producto.nombre}</strong></li>
-      );
-      productosRenderizados.push(
-        <div key={producto.id}> - {producto.descripcion}</div>
-      );
-      nombreActual = producto.nombre;
-    } else {
-      // Mostrar solo la descripción
-      productosRenderizados.push(
-        <div key={producto.id}> - {producto.descripcion}</div>
-      );
-    }
+    let tempProducto = []; //Guarda un arreglo con los productos de un mismo tipo
+    let prodActual = '';
+    productosOrdenados.forEach(element => {
+      if (element.nombre !== prodActual){
+        if (prodActual !== ''){ //Si encuentra un nuevo tipo de producto, hago render de los productos del tipo anterior.
+          productosRenderizados.push( 
+            renderProducto(tempProducto)
+          );
+        }
+        tempProducto = [];
+        tempProducto.push(element);
+        prodActual = element.nombre;
+      } else {
+        tempProducto.push(element);
+      }
+    });
+
+    return productosRenderizados;
+  };
+
+  return (
+    <div className='divProductos'>
+      <h1 className='titulo'>Productos</h1>
+      <ul className='listaProductos'>
+        {renderProductos()}
+      </ul>
+    </div>
+  );
+}
+
+function renderProducto(arrayProducto){
+  let render = [];
+  render.push( //Agrego el titulo (tipo de producto)
+    <li className='tituloProd' key={arrayProducto[0].id}><strong>{arrayProducto[0].nombre}</strong></li>
+  );
+  render.push( //Agrego la descripcion
+    <div className='descProd' key={arrayProducto[0].id}>{arrayProducto[0].descripcion}</div>
+  );
+  arrayProducto.shift() //Quito ese primer elemento del arreglo
+
+  arrayProducto.forEach(element =>{ //Para todos los demas, solo agrego la descripcion
+    render.push(
+      <div className='descProd' key={element.id}>{element.descripcion}</div>
+    );
   });
 
-  return productosRenderizados;
-};
-
-return (
-  <div>
-    <h1>Productos</h1>
-    <ul>
-      {renderProductos()}
-    </ul>
-  </div>
-);
+  return( //Encierra a cada grupo de productos en un div
+    <div className='producto'>
+      {render}
+    </div>
+  );
 }
+
 
 export default App;
